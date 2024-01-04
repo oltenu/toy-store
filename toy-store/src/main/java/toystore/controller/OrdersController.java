6ae3dc5a09d3c13d17d4dc6d1e811d1da979a289
oltenu <oldarius2002@gmail.com>
2024-01-04 22:36:24 +0200

    Implemented Controller

---
+++ toy-store/src/main/java/toystore/controller/OrdersController.java
@@ -0,0 +1,75 @@
+package toystore.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.boot.Banner;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import toystore.model.Order;
+import toystore.model.User;
+import toystore.service.OrderService;
+import toystore.service.ToyService;
+import toystore.service.UserService;
+
+import static toystore.helper.Helper.getHeaderName;
+
+@Controller
+@RequestMapping("orders")
+public class OrdersController {
+
+    private final OrderService orderService;
+    private final UserService userService;
+    private final ToyService toyService;
+
+    public OrdersController(OrderService orderService, UserService userService, ToyService toyService){
+        this.orderService = orderService;
+        this.userService = userService;
+        this.toyService = toyService;
+    }
+
+    @GetMapping("place")
+    public String renderPlaceOrderForm(Model model, Authentication authentication){
+        model.addAttribute("title", "Place Order");
+        model.addAttribute(new Order());
+        model.addAttribute("headerName", getHeaderName(authentication, userService));
+        model.addAttribute("toys", toyService.findAll());
+
+        return "orders/place_order";
+    }
+
+    @PostMapping("place/submit")
+    public String placeOrder(@Valid @ModelAttribute Order order, Model model, Errors errors, Authentication authentication){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Place Order");
+            model.addAttribute(new Order());
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+            model.addAttribute("toys", toyService.findAll());
+
+
+            return "orders/place_order";
+        }
+
+        User user = userService.findUserByEmail(authentication.getName());
+        order.setCustomer(user);
+
+        boolean result = orderService.placeOrder(order);
+
+        if(!result){
+            model.addAttribute("title", "Place Order");
+            model.addAttribute(new Order());
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+            model.addAttribute("errors", "Invalid quantity!");
+            model.addAttribute("toys", toyService.findAll());
+
+
+            return "orders/place_order";
+        }
+
+        return "redirect:http://localhost:8080";
+    }
+}
---
+++ toy-store/src/main/java/toystore/controller/UsersController.java
@@ -0,0 +1,139 @@
+package toystore.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.boot.Banner;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import toystore.model.Role;
+import toystore.model.Toy;
+import toystore.model.User;
+import toystore.service.RoleService;
+import toystore.service.UserService;
+
+import java.util.List;
+
+import static toystore.helper.Helper.getHeaderName;
+
+@Controller
+@RequestMapping("users")
+public class UsersController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public UsersController(UserService userService, RoleService roleService){
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String renderUsersList(Model model, Authentication authentication){
+        model.addAttribute("title", "Users");
+        model.addAttribute("headerName", getHeaderName(authentication, userService));
+        model.addAttribute("users", userService.findAllUsers());
+
+        return "users/users";
+    }
+
+    @GetMapping("details")
+    public String showUserDetails(@RequestParam(required = false) Long userId, Model model, Authentication authentication){
+        if(userId == null){
+            return "redirect:/users";
+        } else {
+            User user = userService.findById(userId);
+
+            model.addAttribute("title", "User: " + user.getFirstName());
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+            model.addAttribute(user);
+            List<Role> roles = roleService.findAllRoles();
+            model.addAttribute("roles", roles);
+
+            return "users/user_details";
+        }
+    }
+
+    @PostMapping("update")
+    public String updateUser(@Valid @ModelAttribute User user, @RequestParam(required = false) long userId, Model model,
+                             Errors errors, Authentication authentication){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Toy: " + user.getFirstName());
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+            model.addAttribute(user);
+            List<Role> roles = roleService.findAllRoles();
+            model.addAttribute("roles", roles);
+
+            return "users/user_details";
+        }
+
+        User existingUser = userService.findUserByEmail(user.getEmail());
+
+        if(existingUser != null){
+            model.addAttribute("title", "Toy: " + user.getFirstName());
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+            model.addAttribute(user);
+            model.addAttribute("errors", "There is already an account with the same email!");
+            List<Role> roles = roleService.findAllRoles();
+            model.addAttribute("roles", roles);
+
+            return "users/user_details";
+        }
+
+        User dbUser = userService.findById(userId);
+        dbUser.setEmail(user.getEmail());
+        dbUser.setFirstName(user.getFirstName());
+        dbUser.setLastName(user.getLastName());
+        dbUser.setRoles(user.getRoles());
+        dbUser.setPassword(user.getPassword());
+        userService.saveUser(dbUser);
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("create")
+    public String renderCreateUserForm(Model model, Authentication authentication){
+        model.addAttribute("title", "Add User");
+        model.addAttribute(new User());
+        model.addAttribute("headerName", getHeaderName(authentication, userService));
+        List<Role> roles = roleService.findAllRoles();
+        model.addAttribute("roles", roles);
+
+        return "users/create_user";
+    }
+
+    @PostMapping("create/save")
+    public String adduser(@Valid @ModelAttribute User user, Model model, Errors errors, Authentication authentication){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Add user");
+            model.addAttribute(user);
+            model.addAttribute("headerName", getHeaderName(authentication, userService));
+
+            return "toys/create_user";
+        }
+
+        userService.saveUser(user);
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("delete")
+    public String renderDeleteForm(Model model, Authentication authentication){
+        model.addAttribute("title", "Delete Users");
+        model.addAttribute("headerName", getHeaderName(authentication, userService));
+        model.addAttribute("users", userService.findAllUsers());
+
+        return "users/delete_user";
+    }
+
+    @PostMapping("delete/submit")
+    public String deleteUsers(@RequestParam(required = false) long[] userIds){
+        if(userIds != null){
+            for(long id : userIds){
+                userService.deleteUser(id);
+            }
+        }
+
+        return "redirect:/users/delete";
+    }
+}
